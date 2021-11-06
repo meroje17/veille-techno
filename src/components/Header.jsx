bf1fdf5b218a680e05772ea4fa7a5310459d5328
Jérôme Guèrin <guerinjerome@outlook.fr>
2021-11-06 13:10:38 +0100

    header base 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 
+// Styles
+import * as naming from '../constants/styles';
+import './Header.css';
+
 const Header = () => (
-    <header>
-        <h1>VEILLE TECHNOLOGIQUE</h1>
+    <header className={naming.HEADER.base}>
+        <h1 className={naming.HEADER.title}>VEILLE TECHNOLOGIQUE</h1>
         <nav></nav>
     </header>
 );
--- src/constants/styles.js
+++ src/constants/styles.js
@@ -1,4 +1,9 @@
 export const NAV_LINK = {
-    active: "nav__link--active",
-    inactive: "nav__link--inactive"
+    active: 'nav__link--active',
+    inactive: 'nav__link--inactive'
+};
+
+export const HEADER = {
+    base: 'header',
+    title: 'header__title'
 };
